9e324c22f3eef5cbf92c11e8707f7a4a56104513
shubhamchambhare654@gmail.com <shubhamchambhare654@gmail.com>
2021-03-28 01:04:48 +0530

    elements added

--- my_portfolio/lib/intro.dart
+++ my_portfolio/lib/intro.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// ignore: unused_import
 import 'package:my_portfolio/contact_me.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Intro extends StatefulWidget {
   Intro({Key key}) : super(key: key);
@@ -12,19 +16,117 @@ class _IntroState extends State<Intro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Shubham's Portfolio"),
+        body: SingleChildScrollView(
+      child: Container(
+        width: 600,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Hi !",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800, height: 1.3, fontSize: 32),
+                  textAlign: TextAlign.justify,
+                ),
+                FaIcon(
+                  FontAwesomeIcons.ad,
+                  size: 30,
+                  color: Colors.black87,
+                ),
+              ],
+            ),
+            Text(" I'm Shubham ",
+                style: TextStyle(
+                    fontWeight: FontWeight.w800, height: 1.3, fontSize: 32),
+                textAlign: TextAlign.justify),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Text(
+                "I help out Spider-man ",
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.7,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () async {
+                      final Uri _emailLaunchUri = Uri(
+                        scheme: 'mailto',
+                        path: 'shubhamchambhare654@gmail.com',
+                      );
+                      launch(_emailLaunchUri.toString());
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      child: Text(
+                        'CONTACT ME',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      child: Text(
+                        'SEE MY RESUME',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.white,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        body: FloatingActionButton.extended(
-          backgroundColor: const Color(0xff03dac6),
-          foregroundColor: Colors.black,
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => Portfolio()));
-            // Respond to button press
-          },
-          icon: Icon(Icons.add),
-          label: Text('EXTENDED'),
-        ));
+      ),
+    ));
   }
 }
